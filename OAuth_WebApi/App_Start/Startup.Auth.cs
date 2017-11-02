d56e2022f0f0ee6bdaa4384402e53b5f73856c88
bruno0pizzatto <bruno0pizzatto@gmail.com>
2017-11-02 18:28:57 -0200

    Primeira versao
    -Avaliando a dificuldade de implementacao de autenticacao OAuth em uma WebAPI

---
+++ OAuth_WebApi/App_Start/Startup.Auth.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using OAuth_WebAPI.Providers;
+using OAuth_WebAPI.Models;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+
+
+namespace OAuth_WebAPI
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions authServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                //Em produção se atentar que devemos usar HTTPS
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/oauth2/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                Provider = new CustomOAuthProviderJwt(),
+                AccessTokenFormat = new CustomJwtFormat("http://localhost:1741/")
+            };
+
+            app.UseOAuthAuthorizationServer(authServerOptions);
+
+
+            var issuer = "http://localhost:1741/";
+            var audience = WebApplicationAccess.WebApplicationAccessList.Select(x => x.Value.ClientId).AsEnumerable();
+            var secretsSymmetricKey = (from x in WebApplicationAccess.WebApplicationAccessList
+                                       select new SymmetricKeyIssuerSecurityTokenProvider(issuer, TextEncodings.Base64Url.Decode(x.Value.SecretKey))).ToArray();
+
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AllowedAudiences = audience,
+                    IssuerSecurityTokenProviders = secretsSymmetricKey
+                });
+        }
+    }
+}
---
+++ OAuth_WebApi/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OAuth_WebAPI.Startup))]
+
+namespace OAuth_WebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
